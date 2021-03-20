d82cbd41427fb1f6ece8a2982e2710c4c8cfd3a3
samiam713 <sdonovan1999@gmail.com>
2021-03-19 20:43:09 -0700

    Initial Commit

---
+++ KeepFit/KeepFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepFitApp.swift
+//  KeepFit
+//
+//  Created by Samuel Donovan on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct KeepFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
