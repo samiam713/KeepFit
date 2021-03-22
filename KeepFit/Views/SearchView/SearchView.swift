31b7d4b6f552eb71f3cbe71cfdd561bcdc909e54
Steven Williams <stevencw@usc.edu>
2021-03-22 00:34:06 -0700

    results instead of users in search

--- KeepFit/Views/SearchView/SearchView.swift
+++ KeepFit/Views/SearchView/SearchView.swift
@@ -34,7 +34,7 @@ struct SearchView: View
                 
                 HStack
                 {
-                    Text("Users")
+                    Text("Results:")
                         .font(.system(size: 20))
                         
                 }.padding()
